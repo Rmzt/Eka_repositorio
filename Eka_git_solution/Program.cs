a17629a0b6836c070d9f766fbb52bf5b6690bf52
Teemu Remes <K2131@student.jamk.fi>
2016-01-19 17:19:06 +0200

    Initial commit.

---
+++ Eka_git_solution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eka_git_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Github!");
+        }
+    }
+}
