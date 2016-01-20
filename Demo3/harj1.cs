5540b091dafb08af220a07c6483f27a97aaa3db4
Teemu Remes <K2131@student.jamk.fi>
2016-01-20 19:10:28 +0200

    Olio2, sis mm Demo3

---
+++ Demo3/harj1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Demo3
+{
+    class Kiuas
+    {
+        //constructor
+        public Kiuas(bool onkopaalla, int lampotila, int kosteusmaara)
+        {
+            onoff = onkopaalla;
+            lampo = lampotila;
+            kosteus = kosteusmaara;
+        }
+
+        public bool Status
+        {
+            get { return onoff; }
+            set { onoff = value; }
+        }
+        public int Kuumuus
+        {
+            get { return lampo; }
+            set { lampo = value; }
+        }
+
+        public int Kosteus
+        {
+            get { return kosteus; }
+            set { kosteus = value; }
+
+        }
+
+        private bool onoff;
+        private int lampo;
+        private int kosteus;
+    }
+    class Harj1
+    {
+        static void Main(string[] args)
+        {
+
+            Kiuas eka = new Kiuas(true, 80, 50);
+
+            Console.WriteLine(" " + eka.Status);
+            Console.WriteLine(" " + eka.Kuumuus);
+            Console.WriteLine(" " + eka.Kosteus);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Olio2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio2
+{
+    /*
+    class Point
+    {
+        public int x;
+        public int y;
+    }*/
+    
+    class Car
+    {
+        //constructor
+        public Car(string modelToSet, int engineSize)
+        {
+            model = modelToSet;
+            cc = engineSize;
+        }
+
+        public int EngineSize
+        {
+            get { return cc; }
+        }
+
+        public string ModelProperty
+        {
+            get { return model; }
+            set { model = value; }
+            
+        }
+
+        // class private members - DO NOT TOUCH
+        private string model;
+        private readonly int cc;
+        private const int maxSpeed = 10;
+    }
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Point coordinate = new Point();
+            coordinate.x = 100;
+            coordinate.y = 50;
+
+            ModifyPoint(coordinate);
+
+            Console.WriteLine("coordinate is: " + coordinate.x + "," + coordinate.y);
+            */
+            
+            Car[] toyotas = new Car[1000];
+            for (int i=0; i<1000; i++)
+            {
+                toyotas[i] = new Car("avensis", 1895);
+            }
+            
+            Car toyota = new Car("avensis", 1895);
+            Car toyota2 = new Car("corolla", 1599);
+            Car toyota3 = new Car("gt86", 1999);
+
+            Console.WriteLine("This toyota model is: " + toyota.ModelProperty);
+            Console.WriteLine("This toyota engine size is: " + toyota.EngineSize);
+            
+            Console.WriteLine("This toyota model is: " + toyota2.ModelProperty);
+            Console.WriteLine("This toyota engine size is: " + toyota2.EngineSize);
+
+            Console.WriteLine("This toyota model is: " + toyota3.ModelProperty);
+            Console.WriteLine("This toyota engine size is: " + toyota3.EngineSize);
+            
+            Console.ReadLine();
+        }
+        /*
+        static void ModifyPoint(Point p)
+        {
+            p.x += 500;
+            p.y += 1500;
+        }
+        */
+    }
+}
